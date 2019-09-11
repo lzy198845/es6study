4cbb1af9ccf8ea3e14a01a18e6d18c11506a99fc
lzy198845 <569229975@qq.com>
2019-09-11 11:06:03 +0800

    es6默认参数
    默认参数，默认参数巧用参数检验，扩展运算符应用（可变参数、数组合并）

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// 入口文件：将其他js文件输入到此处
+
 // import test from "./src/test"
 // import "./src/const"
 // import "./src/scope"
 // import "./src/arrow-function"
-// import "./src/parameter"
-import "./src/proxy"
+import "./src/parameter";
+// import "./src/proxy"
 // test()
